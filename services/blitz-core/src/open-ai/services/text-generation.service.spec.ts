54494d485afa15c27ef1d1830ef673f92fbb160d
Dominic Fernandes <dominic.fernandes@panion.org>
2023-01-08 21:59:29 +0100

    add unit test case for openai

--- services/blitz-core/src/open-ai/services/text-generation.service.spec.ts
+++ services/blitz-core/src/open-ai/services/text-generation.service.spec.ts
@@ -1,12 +1,15 @@
 import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { TextModels } from '../enums/models.enum';
 import { TextGenerationService } from './text-generation.service';
 
 jest.mock('openai', () => ({
   OpenAIApi: jest.fn().mockImplementation(() => ({
-    createCompletion: jest.fn(),
+    createCompletion: jest.fn().mockImplementation(() => ({
+      data: 'OpenAI generated data.',
+    })),
   })),
-  Configuration: jest.fn().mockImplementation(),
+  Configuration: jest.fn(),
 }));
 
 describe('TextGenerationService', () => {
@@ -23,5 +26,15 @@ describe('TextGenerationService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should generate text out from the prompt', async () => {
+    const response = await service.generateTextFromPrompt(
+      'test',
+      1,
+      100,
+      TextModels.DaVinci,
+    );
+    expect(response).toEqual('OpenAI generated data.');
   });
 });
